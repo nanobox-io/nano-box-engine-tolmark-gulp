27ad849f9f4ecdcc569d49aaba7907fe6c67a195
Braxton Huggins <braxton@pagodabox.com>
2015-10-19 14:56:17 -0600

    fix the build test

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -5,8 +5,6 @@ pass "unable to start the $VERSION container" docker run --privileged=true -d --
 
 defer docker kill $UUID
 
-pass "Failed to create /opt/code directory" docker exec $UUID mkdir -p /opt/code
-
 pass "Failed to create /code directory" docker exec $UUID mkdir -p /code
 
 pass "create db dir for pkgsrc" docker exec $UUID mkdir -p /data/var/db
@@ -16,8 +14,6 @@ pass "create dir for environment variables" docker exec $UUID mkdir -p /data/etc
 pass "Failed to update pkgsrc" docker exec $UUID /data/bin/pkgin up -y
 
 pass "Failed to copy test project" docker exec $UUID cp -r /opt/tests/sample-tolmark-gulp/ /opt/code
-
-pass "Failed to list files in /opt/code" docker exec $UUID ls /opt/code
 
 pass "Failed to run prepare script" docker exec $UUID bash -c "cd /opt/engines/tolmark-gulp/bin; PATH=/data/sbin:/data/bin:\$PATH ./prepare '$(payload default-prepare)'"
 
