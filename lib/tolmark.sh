93b8c65251a077d4f131e89a4f637904d2c20c60
Tyler Flint <tylerflint@gmail.com>
2016-09-30 16:19:21 -0600

    Install the correct python package

--- lib/tolmark.sh
+++ lib/tolmark.sh
@@ -37,14 +37,14 @@ package_json_runtime() {
 
 # Install the node runtime along with any dependencies.
 install_runtime_packages() {
-  pkgs=("nginx" "$(nodejs_runtime)" "python27")
+  pkgs=("nginx" "$(nodejs_runtime)" "python-27")
 
   nos_install ${pkgs[@]}
 }
 
 # Uninstall build dependencies
 uninstall_build_packages() {
-  pkgs=("$(nodejs_runtime)" "python27")
+  pkgs=("$(nodejs_runtime)" "python-27")
 
   nos_uninstall ${pkgs[@]}
 }
