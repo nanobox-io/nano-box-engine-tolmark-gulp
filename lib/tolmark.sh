8ae3dfe1e7e26a6fc1dcb5bdef05404a4fb87883
Tyler Flint <tylerflint@gmail.com>
2016-11-18 16:31:04 -0700

    Add support for yarn

--- lib/tolmark.sh
+++ lib/tolmark.sh
@@ -21,7 +21,7 @@ nodejs_default_runtime() {
   packagejs_runtime=$(package_json_runtime)
 
   if [[ "$packagejs_runtime" = "false" ]]; then
-    echo "nodejs-4.2"
+    echo "nodejs-4"
   else
     echo $packagejs_runtime
   fi
@@ -33,6 +33,19 @@ nodejs_default_runtime() {
 #   https://github.com/heroku/heroku-buildpack-nodejs/blob/master/bin/compile#L73
 package_json_runtime() {
   echo "false"
+}
+
+# Determine which dependency manager to use (yarn/npm)
+dep_manager() {
+  echo $(nos_validate \
+    "$(nos_payload "config_dep_manager")" \
+    "string" "$(default_dep_manager)")
+}
+
+# Use yarn as the default dep manager
+default_dep_manager() {
+  # todo: probably need to revert to npm if using an old version
+  echo "yarn"
 }
 
 # Install the node runtime along with any dependencies.
@@ -73,12 +86,22 @@ nodejs_check_runtime() {
   fi
 }
 
-# If the package.json has changed since the previous deploy it should
-# be rebuilt to ensure unused packages are purged.
-npm_rebuild() {
-  if [[ "$(nodejs_check_runtime)" = "false" ]]; then
+# installs npm deps via yarn or npm
+install_npm_deps() {
+  # if yarn is available, let's use that
+  if [[ "$(dep_manager)" = "yarn" ]]; then
+    yarn_install
+  else # fallback to npm (slow)
+    npm_install
+  fi
+}
+
+# install dependencies via yarn
+yarn_install() {
+  if [[ -f $(nos_code_dir)/package.json ]]; then
+
     cd $(nos_code_dir)
-    nos_run_process "Rebuilding npm modules" "npm rebuild"
+    nos_run_process "Installing npm modules" "yarn"
     cd - > /dev/null
   fi
 }
@@ -89,16 +112,6 @@ npm_install() {
 
     cd $(nos_code_dir)
     nos_run_process "Installing npm modules" "npm install"
-    cd - > /dev/null
-  fi
-}
-
-# Prune node modules that are no longer needed
-npm_prune() {
-  if [[ -f $(nos_code_dir)/package.json ]]; then
-
-    cd $(nos_code_dir)
-    nos_run_process "Pruning npm modules" "npm prune"
     cd - > /dev/null
   fi
 }
